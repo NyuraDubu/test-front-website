00df9ea9f16bb2b1d3ca5ab63702e5de9f4692cc
Hugo Denamur <hugo.denamur@epitech.eu>
2023-07-06 01:46:12 +0200

    [+]adding switch mode when you click on the button

---
+++ switch.js
@@ -0,0 +1,9 @@
+var esport_btn = document.getElementById("esport_btn");
+
+function btn_right_clic(){
+    esport_btn.style.left = "194px";
+}
+
+function btn_left_clic(){
+    esport_btn.style.left = "0px";
+}
