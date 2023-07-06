712971d7d035eb332d3c0c61a0aa963933d1d6bb
Hugo Denamur <hugo.denamur@epitech.eu>
2023-07-06 18:59:59 +0200

    [+]add description in switch.jss + adjust the size of the text inside the buttons

--- switch.js
+++ switch.js
@@ -1,10 +1,11 @@
 var esport_btn = document.getElementById("esport_btn");
 
+// Cette fonction permet de changer la couleur du bouton et de le déplacer à droite
 function btn_right_clic(){
     esport_btn.style.left = "50%";
     esport_btn.style.backgroundColor = "#b8f600";
 }
-
+// Cette fonction permet de changer la couleur du bouton et de le déplacer à gauche
 function btn_left_clic(){
     esport_btn.style.left = "0px";
     esport_btn.style.backgroundColor = "#0ab5ed";
