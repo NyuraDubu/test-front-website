19fb6eaafa60a9561204ebfc5b251b4e954a7c2e
Nyura <hugo.denamur@epitech.eu>
2023-07-06 18:21:52 +0200

    [+]add the responsive

--- switch.js
+++ switch.js
@@ -1,9 +1,11 @@
 var esport_btn = document.getElementById("esport_btn");
 
 function btn_right_clic(){
-    esport_btn.style.left = "194px";
+    esport_btn.style.left = "50%";
+    esport_btn.style.backgroundColor = "#b8f600";
 }
 
 function btn_left_clic(){
     esport_btn.style.left = "0px";
+    esport_btn.style.backgroundColor = "#0ab5ed";
 }
